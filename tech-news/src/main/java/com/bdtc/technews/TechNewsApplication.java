261e396398f7da068179cf9777578214cd674e7f
LuisHBeck <beck.henrique.luis@gmail.com>
2024-02-26 20:29:28 -0300

    feat: tech news ms init

---
+++ tech-news/src/main/java/com/bdtc/technews/TechNewsApplication.java
@@ -0,0 +1,13 @@
+package com.bdtc.technews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsApplication.class, args);
+	}
+
+}
