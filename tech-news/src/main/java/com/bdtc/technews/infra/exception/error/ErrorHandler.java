10c5b76d7df3fc052e5839c671b8e85ec4ec0a64
LuisHBeck <beck.henrique.luis@gmail.com>
2024-03-02 22:50:52 -0300

    feat: business rule and invalid http argument exception handlers

--- tech-news/src/main/java/com/bdtc/technews/infra/exception/error/ErrorHandler.java
+++ tech-news/src/main/java/com/bdtc/technews/infra/exception/error/ErrorHandler.java
@@ -1,7 +1,9 @@
 package com.bdtc.technews.infra.exception.error;
 
+import com.bdtc.technews.infra.exception.validation.BusinessRuleException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +13,18 @@ public class ErrorHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity error404Handler() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleError400(MethodArgumentNotValidException exception) {
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest()
+                .body(errors.stream()
+                        .map(ValidationErrorData::new).toList());
+    }
+
+    @ExceptionHandler(BusinessRuleException.class)
+    public ResponseEntity businessRuleExceptionHandler(BusinessRuleException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
     }
 }
