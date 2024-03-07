eb321eb3dee07c0af547640af60f64849a9907f5
LuisHBeck <beck.henrique.luis@gmail.com>
2024-03-07 15:00:48 -0300

    feat: handling backup get method errors

--- tech-news/src/main/java/com/bdtc/technews/infra/exception/error/ErrorHandler.java
+++ tech-news/src/main/java/com/bdtc/technews/infra/exception/error/ErrorHandler.java
@@ -1,6 +1,8 @@
 package com.bdtc.technews.infra.exception.error;
 
 import com.bdtc.technews.infra.exception.validation.BusinessRuleException;
+import com.bdtc.technews.infra.exception.validation.ExceededTheExistingBackupLevelException;
+import com.bdtc.technews.infra.exception.validation.ThereIsNoBackupForThisNewsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -26,5 +28,15 @@ public class ErrorHandler {
     @ExceptionHandler(BusinessRuleException.class)
     public ResponseEntity businessRuleExceptionHandler(BusinessRuleException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(ThereIsNoBackupForThisNewsException.class)
+    public ResponseEntity noBackupForNewsHandler(ThereIsNoBackupForThisNewsException exception) {
+        return ResponseEntity.badRequest().body(new ValidationErrorData("newsId", exception.getMessage()));
+    }
+
+    @ExceptionHandler(ExceededTheExistingBackupLevelException.class)
+    public ResponseEntity noBackupLevelForNewsHandler(ExceededTheExistingBackupLevelException exception) {
+        return ResponseEntity.badRequest().body(new ValidationErrorData("level", exception.getMessage()));
     }
 }
