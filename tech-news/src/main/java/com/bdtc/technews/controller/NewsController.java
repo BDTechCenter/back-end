8fa6c9443ebf0e1332387f2386154cad406abb1d
LuisHBeck <beck.henrique.luis@gmail.com>
2024-03-08 08:21:05 -0300

    refactor: adding typing where var was used in the news controller

--- tech-news/src/main/java/com/bdtc/technews/controller/NewsController.java
+++ tech-news/src/main/java/com/bdtc/technews/controller/NewsController.java
@@ -1,11 +1,12 @@
 package com.bdtc.technews.controller;
 
-import com.bdtc.technews.dto.NewsRequestDto;
-import com.bdtc.technews.dto.NewsUpdateDto;
+import com.bdtc.technews.dto.*;
+import com.bdtc.technews.model.News;
 import com.bdtc.technews.service.news.NewsService;
 import com.bdtc.technews.service.news.backup.NewsBackupService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
@@ -26,62 +27,62 @@ public class NewsController {
 
     @PostMapping
     public ResponseEntity createNews(@ModelAttribute @Valid NewsRequestDto newsRequestDto, UriComponentsBuilder uriBuilder) {
-        var news = newsService.createNews(newsRequestDto);
+        NewsDetailingDto news = newsService.createNews(newsRequestDto);
         var uri = uriBuilder.path("tech-news/news/{id}").build(news.id());
         return ResponseEntity.created(uri).body(news);
     }
 
     @GetMapping("/preview")
     public ResponseEntity getNewsPreview(@PageableDefault() Pageable pageable, @RequestParam(name = "sortByView", required = false, defaultValue = "false") boolean sortByView) {
-        var page = newsService.getNewsPreview(pageable, sortByView);
+        Page<NewsPreviewDto> page = newsService.getNewsPreview(pageable, sortByView);
         return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity getNewsById(@PathVariable UUID id) {
-        var news = newsService.getNewsById(id);
+        NewsDetailingDto news = newsService.getNewsById(id);
         return ResponseEntity.ok(news);
     }
 
     @GetMapping()
     public ResponseEntity getNewsFilteringByTags(@RequestParam(name = "tags") String tags, @PageableDefault() Pageable pageable) {
-        var news = newsService.getNewsPreviewFilteringByTags(pageable, tags);
+        Page<NewsPreviewDto> news = newsService.getNewsPreviewFilteringByTags(pageable, tags);
         return ResponseEntity.ok(news);
     }
 
     @PatchMapping("/{id}/publish")
     public ResponseEntity publishNews(@PathVariable UUID id) {
-        var news = newsService.publishNews(id);
+        NewsDetailingDto news = newsService.publishNews(id);
         return ResponseEntity.ok(news);
     }
 
     @PatchMapping("/{id}/archive")
     public ResponseEntity archiveNews(@PathVariable UUID id) {
-        var news = newsService.archiveNews(id);
+        NewsDetailingDto news = newsService.archiveNews(id);
         return ResponseEntity.ok(news);
     }
 
     @GetMapping("/archived")
     public ResponseEntity getArchivedNews(@PageableDefault() Pageable pageable) {
-        var news = newsService.getArchivedNewsPreview(pageable);
+        Page<NewsPreviewDto> news = newsService.getArchivedNewsPreview(pageable);
         return ResponseEntity.ok(news);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity updateNews(@ModelAttribute NewsUpdateDto updateDto, @PathVariable UUID id) {
-        var news = newsService.updateNews(id, updateDto);
+        NewsDetailingDto news = newsService.updateNews(id, updateDto);
         return ResponseEntity.ok(news);
     }
 
     @GetMapping("/{id}/backup")
     public ResponseEntity getNewsBackup(@PathVariable UUID id, @RequestParam(name = "level") int backupLevel) {
-        var news = newsBackupService.getNewsBackup(id, backupLevel);
+        NewsBackupDto news = newsBackupService.getNewsBackup(id, backupLevel);
         return ResponseEntity.ok(news);
     }
 
     @PutMapping("/{id}/backup/{backupId}/restore")
     public ResponseEntity restoreNewsFromABackup(@PathVariable UUID id, @PathVariable Long backupId) {
-        var news = newsBackupService.restoreNewsFromABackup(id, backupId);
+        NewsDetailingDto news = newsBackupService.restoreNewsFromABackup(id, backupId);
         return ResponseEntity.ok(news);
     }
 }
