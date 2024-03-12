0867092cff4fd3946ac4ce9dad67c33fbec9d2ea
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-03-12 10:23:15 -0300

    feat: auth service init with azure-ad

---
+++ auth-service/src/main/java/com/bdtc/authservice/controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.bdtc.authservice.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/test")
+    public ResponseEntity test(@AuthenticationPrincipal(expression = "claims['name']") String claims) {
+        return ResponseEntity.ok(claims);
+    }
+}
