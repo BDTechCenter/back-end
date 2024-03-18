cdb1b075f213730851729a194123b904964b08a7
LuisHBeck <beck.henrique.luis@gmail.com>
2024-03-18 15:49:49 -0300

    feat: add Bean CorsConfig to cors allowed hosts

---
+++ tech-news/src/main/java/com/bdtc/technews/infra/configs/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.bdtc.technews.infra.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfiguration() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedHeaders("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
