7aa3712acfa7ed2b196a94de8977eeead800117c
LuisHBeck <beck.henrique.luis@gmail.com>
2024-03-20 10:55:51 -0300

    feat: auth client to get nwtUser from tokenJWT

---
+++ tech-news/src/main/java/com/bdtc/technews/http/auth/service/AuthClientService.java
@@ -0,0 +1,18 @@
+package com.bdtc.technews.http.auth.service;
+
+import com.bdtc.technews.dto.UserDto;
+import com.bdtc.technews.http.auth.client.AuthClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthClientService {
+
+    @Autowired
+    private AuthClient authClient;
+
+    public String getNtwUser(String tokenJWT) {
+        UserDto userDetails = authClient.getUserDetails(tokenJWT);
+        return userDetails.ntwUser();
+    }
+}
