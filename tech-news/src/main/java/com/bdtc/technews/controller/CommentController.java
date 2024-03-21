472ab082b9059ec3745ef65c8c3b257fb8a55a2e
LuisHBeck <beck.henrique.luis@gmail.com>
2024-03-21 08:51:26 -0300

    fix: PageableDefault annotation parameters to default sort by id desc

--- tech-news/src/main/java/com/bdtc/technews/controller/CommentController.java
+++ tech-news/src/main/java/com/bdtc/technews/controller/CommentController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,7 +30,7 @@ public class CommentController {
     }
 
     @GetMapping("/{newsId}")
-    public ResponseEntity getNewsComments(@PathVariable UUID newsId, @PageableDefault() Pageable pageable) {
+    public ResponseEntity getNewsComments(@PathVariable UUID newsId, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
         Page<CommentDetailingDto> commentsPage = commentService.getCommentsByNewsId(newsId, pageable);
         return ResponseEntity.ok(commentsPage);
     }
