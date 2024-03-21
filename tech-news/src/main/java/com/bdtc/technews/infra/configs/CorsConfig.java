54745b04960d46c9eb3ddab63750c67872431bf8
LuisHBeck <beck.henrique.luis@gmail.com>
2024-03-21 20:46:12 -0300

    fix: cors configuration for tech-news service

--- tech-news/src/main/java/com/bdtc/technews/infra/configs/CorsConfig.java
+++ tech-news/src/main/java/com/bdtc/technews/infra/configs/CorsConfig.java
@@ -6,19 +6,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfiguration() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
-                        .allowedHeaders("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH");
     }
 }
