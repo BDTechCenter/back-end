6ca07622b73a426934c43f699b2787c4cb3f4f76
LuisHBeck <beck.henrique.luis@gmail.com>
2024-03-21 08:10:04 -0300

    feat: add annotation to enable feign clients for synchronous communication

--- tech-news/src/main/java/com/bdtc/technews/TechNewsApplication.java
+++ tech-news/src/main/java/com/bdtc/technews/TechNewsApplication.java
@@ -2,8 +2,10 @@ package com.bdtc.technews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TechNewsApplication {
 
 	public static void main(String[] args) {
