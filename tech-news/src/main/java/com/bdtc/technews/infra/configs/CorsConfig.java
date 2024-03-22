e15d73eff31d7b542ea30cd0d5f9fe9693dc0776
LuisHBeck <beck.henrique.luis@gmail.com>
2024-03-22 15:03:51 -0300

    fix: change cors allowedOrigins to all

--- tech-news/src/main/java/com/bdtc/technews/infra/configs/CorsConfig.java
+++ tech-news/src/main/java/com/bdtc/technews/infra/configs/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.bdtc.technews.infra.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+//    @Value("${frontend.ip}")
+//    private String frontendIPAddress;
+
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH");
     }
 }
