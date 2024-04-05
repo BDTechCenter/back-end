3a1575121626df76f32805f58e8139c938bbabe2
LuisHBeck <beck.henrique.luis@gmail.com>
2024-04-05 14:00:34 -0300

    refactor: FilterHandler class listToString util function

--- tech-news/src/main/java/com/bdtc/technews/service/news/utils/FilterHandler.java
+++ tech-news/src/main/java/com/bdtc/technews/service/news/utils/FilterHandler.java
@@ -7,32 +7,6 @@ import java.util.List;
 
 @Component
 public class FilterHandler {
-//    private List<String> newsPreviewFilterOptions = List.of("view", "latest", "relevance");
-
-//    public void validateNewsPreviewFilter(String sortBy) {
-//        boolean validOption=false;
-//        for(String option : newsPreviewFilterOptions) {
-//            if(sortBy.equals(option)) {
-//                validOption=true;
-//                break;
-//            }
-//        }
-//        if(!validOption) throw new ConflictInPathParameters("Invalid option for filter! Choose between 'view', 'latest' and 'relevance'!");
-//    }
-//
-//    private List<String> newsByAuthorFilterOptions = List.of("published", "archived");
-//
-//    public void validateNewsByAuthorFilter(String sortBy) {
-//        boolean validOption=false;
-//        for(String option : newsByAuthorFilterOptions) {
-//            if(sortBy.equals(option)) {
-//                validOption=true;
-//                break;
-//            }
-//        }
-////        if(!validOption) throw new ConflictInPathParameters("Invalid option for filter! Choose between 'published' and 'archived'!");
-//        if(!validOption) throw new ConflictInPathParameters(String.format("Invalid option for filter! Choose between %s", buildString(newsByAuthorFilterOptions)));
-//    }
 
     public void validateFilter(List<String> filterOptions, String filter) {
         boolean validOption=false;
@@ -42,10 +16,16 @@ public class FilterHandler {
                 break;
             }
         }
-        if(!validOption) throw new ConflictInPathParameters(String.format("Invalid option for filter! Choose between %s", buildString(filterOptions)));
+        if(!validOption) throw new ConflictInPathParameters(
+                String.format(
+                        "Invalid option for filter! Choose between %s",
+                        listToString(filterOptions)
+                )
+        );
     }
 
-    public String buildString(List<String> list) {
+    // UTILS FOR VALIDATE FILTER
+    public String listToString(List<String> list) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < list.size(); i++) {
             sb.append(list.get(i));
