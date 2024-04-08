3167b776cac7c1c14f68c19bfaeef5f6a9ed5dff
LuisHBeck <beck.henrique.luis@gmail.com>
2024-04-08 10:13:10 -0300

    fix: RoleAuthHandler userRoles list validation

--- tech-news/src/main/java/com/bdtc/technews/service/news/utils/RoleAuthHandler.java
+++ tech-news/src/main/java/com/bdtc/technews/service/news/utils/RoleAuthHandler.java
@@ -1,5 +1,6 @@
 package com.bdtc.technews.service.news.utils;
 
+import com.bdtc.technews.contants.RoleOption;
 import com.bdtc.technews.dto.UserDto;
 import com.bdtc.technews.infra.exception.validation.UnauthorizedByRolesException;
 import org.springframework.stereotype.Component;
@@ -9,15 +10,15 @@ import java.util.List;
 @Component
 public class RoleAuthHandler {
 
-    private List<String> validRoles = List.of("ADMIN", "BDUSER");
+    private List<RoleOption> validRoles = List.of(RoleOption.ADMIN, RoleOption.BDUSER);
 
     public void validateUserRole(UserDto userDto) {
         List<String> userRoles = userDto.roles();
         boolean hasAValidRole = false;
 
-        if(userRoles != null && userRoles.isEmpty()) {
+        if(userRoles != null && !userRoles.isEmpty()) {
             for(String userRole : userRoles) {
-                if(validRoles.contains(userRole)) {
+                if(validRoles.contains(RoleOption.stringToRoleOption(userRole))) {
                     hasAValidRole=true;
                     break;
                 }
