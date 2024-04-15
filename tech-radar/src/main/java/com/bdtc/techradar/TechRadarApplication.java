6cc1020337e3b0f64e26f34dbd15ab35babbb383
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-04-15 10:42:35 -0300

    feat: tech-radar

---
+++ tech-radar/src/main/java/com/bdtc/techradar/TechRadarApplication.java
@@ -0,0 +1,13 @@
+package com.bdtc.techradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechRadarApplication.class, args);
+	}
+
+}
