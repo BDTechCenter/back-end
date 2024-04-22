3859b7cbc93e2772d82bfb49cdfb9e13182abc27
LuisHBeck <beck.henrique.luis@gmail.com>
2024-04-22 15:30:13 -0300

    refactor: remove unused dependencies

--- tech-news/src/main/java/com/bdtc/technews/TechNewsApplication.java
+++ tech-news/src/main/java/com/bdtc/technews/TechNewsApplication.java
@@ -2,12 +2,10 @@ package com.bdtc.technews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.Locale;
 
 @SpringBootApplication
-@EnableFeignClients
 public class TechNewsApplication {
 
 	public static void main(String[] args) {
