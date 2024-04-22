fc71208a4f5de1b2f93ec7b53075c73d51c65ec5
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-04-22 15:28:37 -0300

    feat: auth refactor

--- tech-radar/src/main/java/com/bdtc/techradar/constant/Roles.java
+++ tech-radar/src/main/java/com/bdtc/techradar/constant/Roles.java
@@ -1,6 +1,20 @@
 package com.bdtc.techradar.constant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Roles {
     ADMIN,
-    BDUSER
+    BDUSER;
+
+    public static List<Roles> arrayListToRoleList(ArrayList<String> rolesStringList) {
+        List<Roles> rolesList = new ArrayList<Roles>();
+
+        if(rolesStringList != null && !rolesStringList.isEmpty()){
+            for(String role : rolesStringList) {
+                rolesList.add(Roles.valueOf(role));
+            }
+        }
+        return rolesList;
+    }
 }
