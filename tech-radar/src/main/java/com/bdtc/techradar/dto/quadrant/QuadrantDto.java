7a1a45aaffa5298a70724a74b66a8e428ef9edd7
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-04-29 10:58:36 -0300

    feat: return id quadrant GET

--- tech-radar/src/main/java/com/bdtc/techradar/dto/quadrant/QuadrantDto.java
+++ tech-radar/src/main/java/com/bdtc/techradar/dto/quadrant/QuadrantDto.java
@@ -3,6 +3,7 @@ package com.bdtc.techradar.dto.quadrant;
 import com.bdtc.techradar.model.Quadrant;
 
 public record QuadrantDto(
+        String id,
         String name,
         String title,
         String color,
@@ -12,6 +13,7 @@ public record QuadrantDto(
 ) {
     public QuadrantDto(Quadrant quadrant) {
         this(
+                quadrant.getId(),
                 quadrant.getName(),
                 quadrant.getTitle(),
                 quadrant.getColor(),
