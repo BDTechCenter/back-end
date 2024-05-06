cd735e9aa2e73a2aff0c387d96e09741a1fa854b
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-05-06 16:45:25 -0300

    feat: fixed item flag

--- tech-radar/src/main/java/com/bdtc/techradar/TechRadarApplication.java
+++ tech-radar/src/main/java/com/bdtc/techradar/TechRadarApplication.java
@@ -3,11 +3,13 @@ package com.bdtc.techradar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Locale;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class TechRadarApplication {
 
     public static void main(String[] args) {
